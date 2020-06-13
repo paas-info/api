c330194a7c8a9a64bd0d475ae5054d586ddd9e5d
tom <tom@sapletta.com>
2020-06-13 16:03:47 +0200

    implementation of deployment

---
+++ remote_reinstall.bat
@@ -0,0 +1,4 @@
+cd F:\PycharmProjects\faas-ovh-api\
+py deploy.py api.paas.info remove.sh
+py deploy.py api.paas.info install.sh
+cd F:\PhpstormProjects\paas-info-api\
--- remote_update.bat
+++ remote_update.bat
@@ -1,2 +1,3 @@
 cd F:\PycharmProjects\faas-ovh-api\
 py deploy.py api.paas.info update.sh
+cd F:\PhpstormProjects\paas-info-api\
